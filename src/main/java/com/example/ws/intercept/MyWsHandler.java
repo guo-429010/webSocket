c45ab78367df017a2eb91d5828759193a2f6f9c0
guo <1309042148@qq.com>
2023-04-13 11:00:42 +0800

    完成聊天室

--- src/main/java/com/example/ws/intercept/MyWsHandler.java
+++ src/main/java/com/example/ws/intercept/MyWsHandler.java
@@ -23,9 +23,11 @@ public class MyWsHandler extends AbstractWebSocketHandler {
 
     private static Map<String, SessionBean> sessionBeanMap;
     private static AtomicInteger clientIdMaker;
+    private static StringBuffer stringBuffer;
     static {
         sessionBeanMap = new ConcurrentHashMap<>();
         clientIdMaker = new AtomicInteger(0);
+        stringBuffer = new StringBuffer();
     }
 
     //链接建立
@@ -35,6 +37,8 @@ public class MyWsHandler extends AbstractWebSocketHandler {
         SessionBean sessionBean = new SessionBean(session,clientIdMaker.getAndIncrement());
         sessionBeanMap.put(session.getId(),sessionBean);
         log.info(sessionBeanMap.get(session.getId()).getClientId() + "建立了链接");
+        stringBuffer.append(sessionBeanMap.get(session.getId()).getClientId() + "进入了群聊<br/>");
+        sendMessage(sessionBeanMap);
     }
 
     //收到消息
@@ -42,6 +46,8 @@ public class MyWsHandler extends AbstractWebSocketHandler {
     protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
         super.handleTextMessage(session, message);
         log.info(sessionBeanMap.get(session.getId()).getClientId() + ":" + message.getPayload());
+        stringBuffer.append(sessionBeanMap.get(session.getId()).getClientId() + ":" + message.getPayload() + "<br/>");
+        sendMessage(sessionBeanMap);
     }
 
     //传输异常
@@ -58,14 +64,29 @@ public class MyWsHandler extends AbstractWebSocketHandler {
     @Override
     public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
         super.afterConnectionClosed(session, status);
-        log.info(sessionBeanMap.get(session.getId()).getClientId() + "关闭了链接");
+        int clientId = sessionBeanMap.get(session.getId()).getClientId();
+        sessionBeanMap.remove(session);
+        log.info(clientId + "关闭了链接");
+        stringBuffer.append(clientId + "退出了群聊<br/>");
+        sendMessage(sessionBeanMap);
     }
 
-    //每2秒发送一次心跳
-    @Scheduled(fixedRate = 2000)
-    public void sendMsg() throws IOException {
-        for (String key:sessionBeanMap.keySet()){
-            sessionBeanMap.get(key).getWebSocketSession().sendMessage(new TextMessage("心跳"));
+//    //每2秒发送一次心跳
+//    @Scheduled(fixedRate = 2000)
+//    public void sendMsg() throws IOException {
+//        for (String key:sessionBeanMap.keySet()){
+//            sessionBeanMap.get(key).getWebSocketSession().sendMessage(new TextMessage("心跳"));
+//        }
+//    }
+
+    public void sendMessage(Map<String,SessionBean> sessionBeanMap) {
+        for(String key:sessionBeanMap.keySet()){
+            try {
+                sessionBeanMap.get(key).getWebSocketSession().sendMessage(new TextMessage(stringBuffer.toString()));
+            } catch (IOException e) {
+                e.printStackTrace();
+                log.error(e.getMessage());
+            }
         }
     }
 }
